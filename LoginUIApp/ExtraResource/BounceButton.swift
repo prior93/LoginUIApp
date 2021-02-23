3c9964527a7a744ff2233cf13bbaa1b1d1e0ce7d
prior93 <Parashar.adhikary@gmail.com>
2021-02-23 22:04:12 +0200

    UIAPP

---
+++ LoginUIApp/ExtraResource/BounceButton.swift
@@ -0,0 +1,20 @@
+//
+//  BounceButton.swift
+//  LoginUIApp
+//
+//  Created by parashar.r.adhikary on 23/02/2021.
+//
+
+import UIKit
+
+class BounceButton: UIButton {
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 6, options: .allowUserInteraction, animations: {
+            self.transform = CGAffineTransform.identity
+        }, completion: nil)
+        super.touchesBegan(touches, with: event)
+    }
+}
---
+++ LoginUIApp/ExtraResource/DesignButton.swift
@@ -0,0 +1,30 @@
+//
+//  DesignButton.swift
+//  LoginUIApp
+//
+//  Created by parashar.r.adhikary on 23/02/2021.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable class DesignButton: BounceButton {
+    
+    @IBInspectable var borderWidth: CGFloat = 0.0 {
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    @IBInspectable var cornerRadius: CGFloat = 0.0 {
+        didSet{
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+ 
+}
